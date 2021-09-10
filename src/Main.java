5000e86f3cb75ba70dbd149856f73e9b03b3fdab
Yuliya Skripkina <yu.skripkina@gmail.com>
2021-09-11 00:17:16 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int amount = 500000000;
+        int total = balance + amount;
+        System.out.println(total);
+        }
+    }
